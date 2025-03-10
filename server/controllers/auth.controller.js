100047d73ef87b16b22c106dba32be7e59f853a9
Asadullah325 <asadullah456545@gmail.com>
2025-03-11 01:02:22 +0500

    Login route Created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -36,3 +36,40 @@ export const register = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const isPasswordValid = await user.comparePassword(password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        res.status(200).json({
+            message: "Login successful",
+            token: user.generateJWT(),
+            userId: user._id
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -44,6 +44,10 @@ userSchema.pre("save", async function (next) {
     }
 });
 
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
 userSchema.methods.generateJWT = function () {
     return jwt.sign({
         _id: this._id,
